c5bdbfc7c39246ad7ab598b6ccc80e0db3034180
Kevin Aspesi <kta221@lehigh.edu>
2020-11-02 10:39:23 -0500

    Enabled JWT auth for upload route

--- routers/investigateRouter.js
+++ routers/investigateRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const multer  = require('multer')
 const upload = multer({ dest: 'images/' })
+const auth = require("../middleware/auth");
 const { investigateImage } = require('../controllers/investigateController');
 
 const investigateRouter = express.Router();
 
-investigateRouter.post('/', upload.array('photos', 12), investigateImage);
+investigateRouter.post('/', auth, upload.array('photos', 12), investigateImage);
 
 module.exports = investigateRouter;
