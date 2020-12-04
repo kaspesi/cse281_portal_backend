6479c18033950f6c843a51203ade6a1dbc271bf1
Kevin Aspesi <kta221@lehigh.edu>
2020-12-03 20:13:01 -0500

    1

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.listen(PORT, () => {
 // });
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	res.sendFile(path.join(__dirname+'client/build/index.html'));
   });
 
 //Connect DB
