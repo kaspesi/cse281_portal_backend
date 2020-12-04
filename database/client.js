0f5c95262bf3b29a6ac061e257ed657646ed560e
Kevin Aspesi <kta221@lehigh.edu>
2020-12-04 17:37:23 -0500

    Fix

--- database/client.js
+++ database/client.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 
 const client = new Client({
-	user: 'igonyfvwvdafir',
-	host: 'ec2-34-192-122-0.compute-1.amazonaws.com',
-	database: 'd3al9v8lhdukc',
-	password: 'ba2c060d39ea7ef7aad3b56cce7584dbab365a8ecf6c4e6e0e59143c5b62ac95',
+	user: '',
+	host: '',
+	database: '',
+	password: '',
 	port: 5432,
 	ssl: { rejectUnauthorized: false }
 });
